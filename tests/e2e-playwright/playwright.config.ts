6a1221e8430fd6bafb5847bc73d454b9fa88f700
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-12-12 19:14:37 +0600

    Remove commented code

--- tests/e2e-playwright/playwright.config.ts
+++ tests/e2e-playwright/playwright.config.ts
@@ -11,9 +11,6 @@
      path.join( process.cwd(), 'artifacts/storage-states/admin.json' );
  
  const config: PlaywrightTestConfig = {
-    //  reporter: process.env.CI
-    //      ? [ [ 'github' ], [ './config/flaky-tests-reporter.ts' ] ]
-    //      : 'list',
      reporter: [
          ["html", { open: "never" }],
          ["junit", { outputFile: "playwright-report/results.xml" }],
